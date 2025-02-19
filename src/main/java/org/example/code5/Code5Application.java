402007d34e5d291eafead99f064c240307d32b70
Alexandr Sychev <netovarish@yandex.ru>
2025-02-19 20:36:39 +0300

    первыйзадача 2.1.3

---
+++ src/main/java/org/example/code5/Code5Application.java
@@ -0,0 +1,13 @@
+package org.example.code5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Code5Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/code5/Code5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.code5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Code5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
